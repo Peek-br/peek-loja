573741a7a3f43d81251755344dc02073c5e1927d
Peek-br <peek.br.ai@gmail.com>
2026-09-15 01:43:09 -0300

    Update SHEET_CSV_URL with published link

--- config.js
+++ config.js
@@ -7,4 +7,4 @@
 //   3. Escolha a aba certa e o formato "Valores separados por vírgula (.csv)"
 //   4. Clique em "Publicar", copie o link gerado e cole abaixo, entre aspas.
 
-const SHEET_CSV_URL = "COLE_AQUI_O_LINK_CSV_PUBLICADO_DA_SUA_PLANILHA";
+const SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQgKtCbq8Dhn0o4DNqUTqPNtAF23YAsF7kplFop06EoCMWsZERJvRu4ZLW8WtoYijNTzUJj6a-yOd2-/pubhtml?gid=2015392480&single=true";
